170159581657de50f97a1217f67daf02c2ed60fa
Bruce Han <intragerhan@gmail.com>
2023-04-24 23:39:44 +0900

    Time: 1310 ms (9.38%), Space: 0B (100.00%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,4 +1,4 @@
-DELETE FROM Person p
- WHERE p.id <> (SELECT MIN(id) 
-                  FROM Person 
-                 WHERE email = p.email)
+DELETE FROM Person
+ WHERE id NOT IN (SELECT MIN(id)
+                    FROM Person
+                   GROUP BY email)
