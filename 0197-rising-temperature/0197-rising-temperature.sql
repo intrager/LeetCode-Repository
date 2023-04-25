3ccecaea26fcedc69b832f948593bf31a378db73
Bruce Han <intragerhan@gmail.com>
2023-04-25 23:33:44 +0900

    Time: 940 ms (36.60%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,4 +1,8 @@
-SELECT w1.id
-  FROM Weather w1, Weather w2
- WHERE w1.recordDate - 1 = w2.recordDate
-   AND w1.temperature > w2.temperature;
+SELECT t.id AS ID
+  FROM (SELECT w.id
+               , w.temperature AS 기준온도
+               , w.recordDate - LAG(w.recordDate, 1) OVER(ORDER BY w.recordDate) AS 최솟값
+               , LAG(w.temperature, 1) OVER(ORDER BY w.recordDate) AS 온도
+          FROM Weather w) t
+ WHERE 최솟값 = 1
+   AND t.기준온도 > t.온도
