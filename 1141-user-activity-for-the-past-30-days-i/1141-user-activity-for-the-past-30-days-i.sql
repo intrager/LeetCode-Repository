e3a0d597c9109a6e3526d39d0b14ad836ed0e921
Bruce Han <intragerhan@gmail.com>
2023-05-11 22:06:43 +0900

    Time: 681 ms (84.00%), Space: 0B (100.00%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -2,3 +2,4 @@ SELECT TO_CHAR(activity_date, 'YYYY-MM-DD') AS day, COUNT(DISTINCT user_id) AS a
   FROM Activity
  WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
  GROUP BY activity_date
+  HAVING COUNT(session_id) > 0
