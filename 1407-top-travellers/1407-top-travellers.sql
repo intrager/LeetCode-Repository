93a96f33a2bff9f11a8a2e2d18dde37025860e2c
Bruce Han <intragerhan@gmail.com>
2023-05-19 23:32:36 +0900

    Time: 1242 ms (58.82%), Space: 0B (100.00%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -1,4 +1,4 @@
-SELECT U.name, COALESCE(SUM(R.distance), 0) AS travelled_distance
+SELECT U.name, NVL(SUM(R.distance), 0) AS travelled_distance
 FROM Users U
 LEFT JOIN Rides R ON U.id = R.user_id 
 GROUP BY R.user_id, U.name
